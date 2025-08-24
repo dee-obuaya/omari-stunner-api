24772630766ef99fe8174026b84f2df5bf0928d2
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-08-24 16:30:28 +0100

    update: modified model schema and corrected export

--- models/tab.js
+++ models/tab.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TabSchema = new Schema({
-    tab: String,
+    name: String,
+    label: String
 });
 
-module.exports = mongoose.model('Service', TabSchema);
+module.exports = mongoose.model('Tab', TabSchema);
