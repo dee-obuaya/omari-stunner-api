b480b6de7f6ed0b9ce2ec26b62c9d568df42ed8e
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-09-30 14:33:41 +0100

    update schema

--- models/service.js
+++ models/service.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ServiceSchema = new Schema({
-    service: String,
-    tag: String,
-    price: Number,
+    service: {
+        type: String,
+        required: [true, 'Service name cannot be blank']
+    },
+    tag: {
+        type: String,
+        required: [true, 'Service tag cannot be blank'],
+        lowercase: true,
+        enum: ['makeup', 'lashes', 'brows']
+    },
+    price: {
+        type: Number,
+        min: 0
+    },
 });
 
 module.exports = mongoose.model('Service', ServiceSchema);
--- models/tab.js
+++ models/tab.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TabSchema = new Schema({
-    name: String,
-    label: String
+    name: {
+        type: String,
+        required: [true, 'Tab name cannot be blank']
+    },
+    label: {
+        type: String,
+        required: [true, 'Tab label cannot be blank']
+    },
 });
 
 module.exports = mongoose.model('Tab', TabSchema);
