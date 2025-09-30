c32fce72fafd4773c3bd18330122b5d6c47d63ef
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-09-30 14:30:49 +0100

    update: route restructure

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+if (process.env.NODE_ENV !=='production') {
+    require('dotenv').config();
+};
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Service = require('./models/service');
-const Tab = require('./models/tab');
 
-mongoose.connect('mongodb://localhost:27017/omari-stunner-dev');
+
+const dbName = process.env.DB;
+const ExpressError = require('./utils/ExpressError');
+
+const services = require('./routes/services');
+const images = require('./routes/images');
+const tabs = require('./routes/tabs');
+
+mongoose.connect(`mongodb://localhost:27017/${dbName}`);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,27 +23,62 @@ db.once('open', () => {
 });
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 // Configure CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your React app's origin
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/api/services', services)
+
+app.use('/api/images', images);
+
+app.use('/api/tabs', tabs);
+// if setting /:id here, got to route file and add {mergeParams: true} to express.Router(add here)
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Omari Stunner!');
 });
 
-app.get('/api/services', async (req, res) => {
-    const services = await Service.find({});
-    res.json(services);
+const handleValidationError = err => {
+    return new ExpressError(400, `Validation Failed: ${err.message}`);
+};
+
+const handleCastError = err => {
+    return new ExpressError(400, `Invalid entry: ${err.message}`);
+};
+
+app.all(/(.*)/, (req, res, next) => {
+    next(new ExpressError(404, 'Page Not Found'));
 });
 
-app.get('/api/tabs', async (req, res) => {
-    const tabs = await Tab.find({});
-    res.json(tabs);
+app.use((err, req, res, next) => {
+    console.log(err);
+    switch (err.name) {
+        case 'ValidationError':
+            err = handleValidationError(err);
+            break;
+        case 'CastError':
+            err = handleCastError(err);
+            break;
+        default:
+            break;
+    };
+
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    res.status(statusCode).send({message: message});
 });
 
 app.listen(port, () => {
