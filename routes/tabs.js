94d11a86201f0615604bf29fdfa7cadbf3fd7234
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-10-22 10:29:31 +0100

    update to add routes for add, update, and delete

--- routes/tabs.js
+++ routes/tabs.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const tabs = require('../controllers/tabs');
 const handleAsync = require('../utils/handleAsync');
+const {validateTab} = require('../utils/middleware');
 
 
-router.get('/', handleAsync(tabs.index));
+router.route('/')
+    .get(handleAsync(tabs.index))
+    .post(validateTab, handleAsync(tabs.addTab));
+
+router.route('/:id')
+    .put(validateTab, handleAsync(tabs.updateTab))
+    .delete(handleAsync(tabs.deleteTab));
 
 module.exports = router;
