b38a06533d1b2a7fdd6e3643349c13aab97e1482
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-11-05 15:00:36 +0100

    update: add handleLogin and ensureAuthenticated

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,6 @@
 const {serviceSchema, imageSchema, tabSchema, bookingSchema} = require('./joiSchemas');
 const ExpressError = require('./ExpressError');
+const passport = require('passport');
 
 module.exports.validateService = (req, res, next) => {
     const { error } = serviceSchema.validate(req.body);
@@ -36,7 +37,7 @@ module.exports.validateTab = (req, res, next) => {
     if (error) {
         // console.log(error);
         const msg = error.details.map(el => el.message).join(',');
-        console.log(msg);
+        // console.log(msg);
         res.status(400).json({message: msg});
         throw new ExpressError(400, msg);
     } else {
@@ -51,10 +52,44 @@ module.exports.validateBooking = (req, res, next) => {
     if (error) {
         // console.log(error);
         const msg = error.details.map(el => el.message).join(',');
-        console.log(msg);
+        // console.log(msg);
         res.status(400).json({message: msg});
         throw new ExpressError(400, msg);
     } else {
         next();
     }
+};
+
+module.exports.handleLogin = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return next(err);
+
+        if (!user) {
+            // Authentication failed
+            return res.status(401).json({
+                success: false,
+                message: info?.message || 'Invalid credentials',
+            });
+        }
+
+        // Log the user in manually
+        req.logIn(user, (err) => {
+            if (err) return next(err);
+
+            // Attach user for next middleware
+            req.authenticatedUser = {
+                // id: user.id,
+                name: user.name,
+                role: user.role,
+            };
+
+            // 👇🏽 Hand control to controller
+            next();
+        });
+    })(req, res, next);
+};
+
+module.exports.ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    return res.status(401).json({ success: false, message: 'Session expired. Please log in again.' });
 };
