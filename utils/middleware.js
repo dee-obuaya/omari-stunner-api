9999e543b90a2d740c9ef6cffc4e3552184f99db
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-11-25 15:49:26 +0100

    updat: add message submition validation middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,4 @@
-const {serviceSchema, imageSchema, tabSchema, bookingSchema} = require('./joiSchemas');
+const {serviceSchema, imageSchema, tabSchema, bookingSchema, messageSchema} = require('./joiSchemas');
 const ExpressError = require('./ExpressError');
 const passport = require('passport');
 
@@ -53,6 +53,21 @@ module.exports.validateBooking = (req, res, next) => {
         // console.log(error);
         const msg = error.details.map(el => el.message).join(',');
         // console.log(msg);
+        res.status(400).json({message: msg});
+        throw new ExpressError(400, msg);
+    } else {
+        next();
+    }
+};
+
+module.exports.validateMessage = (req, res, next) => {
+    const { message } = req.body.message;
+    const { error } = messageSchema.validate(message);
+
+    if (error) {
+        // console.log(error);
+        const msg = error.details.map(el => el.message).join(',');
+
         res.status(400).json({message: msg});
         throw new ExpressError(400, msg);
     } else {
